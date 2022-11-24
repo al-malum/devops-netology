2395f2d54a296e75978f327d889a4f14b36d826f
Alexander Marchenko <a.malum.rr@gmail.com>
2022-11-24 18:06:55 +0300

    Доработка

--- sql/SQL-Часть 1.sql
+++ sql/SQL-Часть 1.sql
@@ -1,23 +1,21 @@
--- Марченко А.А.;
-
-
 -- 1 задание
 -- Получите уникальные названия районов из таблицы с адресами, которые начинаются на “K” и заканчиваются на “a”, и не содержат пробелов.
 SELECT DISTINCT(district) 
 FROM address AS a 
-WHERE LOWER(district) LIKE 'k%a' AND LOWER(district) NOT LIKE ' ';
+WHERE LOWER(district) LIKE 'k%a' AND LOWER(district) NOT LIKE '% %';
 
 -- 2 задание
 -- Получите из таблицы платежей за прокат фильмов информацию по платежам,
 -- оторые выполнялись в промежуток с 15 июня 2005 года по 18 июня 2005 года включительно, 
 -- и стоимость которых превышает 10.00.
 SELECT * FROM payment AS p
-WHERE DATE(payment_date) BETWEEN '2005-06-15' AND '2005-06-18' AND amount > 10;
+WHERE DATE(payment_date) BETWEEN '2005-06-15' AND '2005-06-18' AND amount > 10
+ORDER BY payment_date DESC;
 
 -- 3 задание
 -- Получите последние 5 аренд фильмов.
-SELECT payment_date from payment 
-ORDER BY payment_date 
+SELECT rental_date from rental 
+ORDER BY rental_date
 DESC LIMIT 0, 5;
 
 
@@ -33,8 +31,7 @@ WHERE LOWER(first_name) = 'kelly' OR LOWER(first_name) = 'willie'
 -- Выведите Email каждого покупателя, разделив значение Email на 2 отдельных колонки:
 -- в первой колонке должно быть значение, указанное до @, во второй значение, указанное после @.
 SELECT email, 
-		SUBSTR(email, 1, (POSITION('@' IN email) - 1)),
-		SUBSTR(email, POSITION('@' IN email) + 1)
+                SUBSTR(email, 1, (POSITION('@' IN email) - 1)),
+                SUBSTR(email, POSITION('@' IN email) + 1)
 FROM customer;
-
 
